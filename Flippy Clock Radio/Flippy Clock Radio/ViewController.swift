f3853c036f4cf7103fdfde39b4c784df05c54af9
jonathankofahl <jonathankofahl@posteo.de>
2020-07-15 21:40:39 +0200

    set UI Layout

--- Flippy Clock Radio/Flippy Clock Radio/ViewController.swift
+++ Flippy Clock Radio/Flippy Clock Radio/ViewController.swift
@@ -9,6 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: - OUTLETS
+       @IBOutlet weak var imageView: UIImageView!
+       @IBOutlet weak var buttonView: UIView!
+       @IBOutlet weak var offButton: UIButton!
+       @IBOutlet weak var button1: UIButton!
+       @IBOutlet weak var button2: UIButton!
+       @IBOutlet weak var button3: UIButton!
+       @IBOutlet weak var radioLabel1: UILabel!
+       @IBOutlet weak var radioLabel2: UILabel!
+       @IBOutlet weak var radioLabel3: UILabel!
+       @IBOutlet weak var slider: UISlider!
+       @IBOutlet weak var labelVolume: UILabel!
+       @IBOutlet weak var settingsButton: UIButton!
+       @IBOutlet weak var clock1: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
