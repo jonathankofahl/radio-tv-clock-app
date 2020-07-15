d66c1b8f7c6a9d8a6fe311c083f53a740dcd598a
jonathankofahl <jonathankofahl@posteo.de>
2020-07-15 21:04:04 +0200

    init project

---
+++ Flippy Clock Radio/Flippy Clock Radio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flippy Clock Radio
+//
+//  Created by Jonathan Kofahl on 15.07.20.
+//  Copyright © 2020 JonathanKofahl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
