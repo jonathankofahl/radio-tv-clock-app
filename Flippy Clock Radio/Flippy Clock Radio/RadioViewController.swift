b5992bf3374a03aae5a59f9fbb0513cd3d58f49b
jonathankofahl <jonathankofahl@posteo.de>
2020-07-16 14:16:18 +0200

    added basic settingsViewController-functionality

--- Flippy Clock Radio/Flippy Clock Radio/RadioViewController.swift
+++ Flippy Clock Radio/Flippy Clock Radio/RadioViewController.swift
@@ -13,17 +13,18 @@ import MediaPlayer
 class RadioViewController: UIViewController {
     
     //MARK: - OUTLETS
-       @IBOutlet weak var imageView: UIImageView!
-       @IBOutlet weak var buttonView: UIView!
-       @IBOutlet weak var button1: UIButton!
-       @IBOutlet weak var button2: UIButton!
-       @IBOutlet weak var button3: UIButton!
-       @IBOutlet weak var slider: UISlider!
-       @IBOutlet weak var settingsButton: UIButton!
-       @IBOutlet weak var clock: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var buttonView: UIView!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var settingsButton: UIButton!
+    @IBOutlet weak var clock: UILabel!
     
     //MARK: - VARIABLES
     var clockTimer: Timer?
+    var dateFormat: Bool = true
     var player : AVPlayer?
     var isPlaying: Bool = false
     var radioURL1: String?
@@ -31,29 +32,63 @@ class RadioViewController: UIViewController {
     var radioURL3: String?
     var activeRadioTag: Int?
     var volume: Float = 0.5
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         showTime()
         
         /// refreseh clock
         self.clockTimer =  Timer.scheduledTimer(timeInterval: 10.0, target: self, selector: #selector(self.showTime), userInfo: nil, repeats: true)
-
+        
+        updateView()
     }
-
+    
+    func updateView() -> Void {
+        /// Clock Format
+        let format = UserDefaults.standard.bool(forKey: "clockDate")
+        dateFormat = format
+        
+        /// Radio Streams
+        if (UserDefaults.standard.object(forKey: "radioLink0") != nil) {
+            self.radioURL1 = UserDefaults.standard.string(forKey: "radioLink0")
+        }
+        if (UserDefaults.standard.object(forKey: "radioLink1") != nil) {
+            self.radioURL2 = UserDefaults.standard.string(forKey: "radioLink1")
+        }
+        if (UserDefaults.standard.object(forKey: "radioLink2") != nil) {
+            self.radioURL3 = UserDefaults.standard.string(forKey: "radioLink2")
+        }
+        /// init Radio Names
+        if (UserDefaults.standard.object(forKey: "radioName0") != nil) {
+            self.button1.setTitle(UserDefaults.standard.string(forKey: "radioName0"), for: UIControl.State.normal)
+        }
+        if (UserDefaults.standard.object(forKey: "radioName1") != nil) {
+            self.button2.setTitle(UserDefaults.standard.string(forKey: "radioName1"), for: UIControl.State.normal)
+        }
+        if (UserDefaults.standard.object(forKey: "radioName2") != nil) {
+            self.button3.setTitle(UserDefaults.standard.string(forKey: "radioName2"), for: UIControl.State.normal)
+        }
+        
+        ///set BackgroundImage
+        self.imageView.image = loadImageFromDiskWith(fileName: "image.png") ?? UIImage(named: "sample_background")
+    }
+    
     //MARK: - Methods
     @objc func showTime() -> Void {
-        // get the current date and time
+        /// get the current date and time
         let currentDateTime = Date()
-        // initialize the date formatter and set the style
+        /// initialize the date formatter and set the style
         let formatter = DateFormatter()
         formatter.timeStyle = .short
         formatter.dateStyle = .medium
+        if !dateFormat {
+            formatter.dateStyle = .none
+        }
         self.clock!.text = formatter.string(from: currentDateTime)
     }
     
     @IBAction func tapPiece(_ gestureRecognizer : UITapGestureRecognizer ) {
-    guard gestureRecognizer.view != nil else { return }
+        guard gestureRecognizer.view != nil else { return }
         if self.buttonView.isHidden {
             self.buttonView.isHidden = false
             self.slider.isHidden = false
@@ -65,7 +100,7 @@ class RadioViewController: UIViewController {
         self.slider.isHidden = true
         self.settingsButton.isHidden = true
     }
-
+    
     
     //MARK: Radio Methods
     @IBAction func playMedia(_ sender: UIButton) {
--- Flippy Clock Radio/Flippy Clock Radio/SettingsViewController.swift
+++ Flippy Clock Radio/Flippy Clock Radio/SettingsViewController.swift
@@ -7,19 +7,112 @@
 //
 
 import UIKit
+import AVFoundation
+import UserNotifications
 
-class SettingsViewController: UITableViewController {
-
+class SettingsViewController: UITableViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    //MARK: - OUTLETS
+    @IBOutlet weak var dateSwitch: UISwitch!
+    @IBOutlet weak var displaySwitch: UISwitch!
+    @IBOutlet weak var radioNameLabel1: UITextField!
+    @IBOutlet weak var radioNameLabel2: UITextField!
+    @IBOutlet weak var radioNameLabel3: UITextField!
+    @IBOutlet weak var radioLinkLabel1: UITextField!
+    @IBOutlet weak var radioLinkLabel2: UITextField!
+    @IBOutlet weak var radioLinkLabel3: UITextField!
+    
+    
+    //MARK: - OVERRIDE FUNCTIONS
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        ///Load values from UserDefaults
+        /// Clock Format
+        var bool = UserDefaults.standard.bool(forKey: "clockDate")
+        dateSwitch.isOn = bool
+        
+        /// Display Mode
+        bool = UserDefaults.standard.bool(forKey: "displayMode")
+        displaySwitch.isOn = bool
+        
+        
+        /// init Radio Streams
+        if (UserDefaults.standard.object(forKey: "radioLink0") != nil) {
+            self.radioLinkLabel1.text = UserDefaults.standard.string(forKey: "radioLink0")
+        }
+        if (UserDefaults.standard.object(forKey: "radioLink1") != nil) {
+            self.radioLinkLabel2.text = UserDefaults.standard.string(forKey: "radioLink1")
+        }
+        if (UserDefaults.standard.object(forKey: "radioLink2") != nil) {
+            self.radioLinkLabel3.text = UserDefaults.standard.string(forKey: "radioLink2")
+        }
+        
+        /// init Radio Names
+        if (UserDefaults.standard.object(forKey: "radioName0") != nil) {
+            self.radioNameLabel1.text = UserDefaults.standard.string(forKey: "radioName0")
+        }
+        if (UserDefaults.standard.object(forKey: "radioName1") != nil) {
+            self.radioNameLabel2.text = UserDefaults.standard.string(forKey: "radioName1")
+        }
+        if (UserDefaults.standard.object(forKey: "radioName2") != nil) {
+            self.radioNameLabel3.text = UserDefaults.standard.string(forKey: "radioName2")
+        }
+        
     }
-
-   
-
+    
+    //MARK: - CUSTOM FUNCTIONS
+    
+    @IBAction func changeRadioName(_ sender: UITextField) {
+        UserDefaults.standard.set(sender.text, forKey: "radioName"+sender.tag.description)
+    }
+    
+    @IBAction func changeRadioLink(_ sender: UITextField) {
+        UserDefaults.standard.set(sender.text, forKey: "radioLink"+sender.tag.description)
+    }
+    
+    @IBAction func openImages(_ sender: UIButton) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.modalPresentationStyle = .popover
+        let presentationController = imagePicker.popoverPresentationController
+        presentationController?.sourceView = sender
+        self.present(imagePicker, animated: true) {}
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            let fileName = "image.png"
+            saveImage(imageName: fileName, image: pickedImage)
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func changeDateFormat(_ sender: UISwitch) {
+        UserDefaults.standard.set(sender.isOn, forKey: "clockDate")
+    }
+    
+    @IBAction func changeDisplayMode(_ sender: UISwitch) {
+        UserDefaults.standard.set(sender.isOn, forKey: "displayMode")
+    }
+    
+    @IBAction func saveSettings(_ sender: Any) {
+        self.dismiss(animated: true, completion: {})
+    }
+    
+    
+    //MARK: - helper Methods
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
 }
