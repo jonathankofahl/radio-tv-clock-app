349300f92c34f3c183381709f5b806c88e8d63b9
Jonathan Kofahl <jonathankofahl@posteo.de>
2020-07-30 12:39:03 +0200

    prevent display lock

--- Flippy Clock Radio/Flippy Clock Radio/AppDelegate.swift
+++ Flippy Clock Radio/Flippy Clock Radio/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        /// prevent automatic display lock
+        UIApplication.shared.isIdleTimerDisabled = true
+
         return true
     }
     
